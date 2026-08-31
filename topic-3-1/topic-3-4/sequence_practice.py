0ee154d789e8221593d9e8efa3aef40121b277e7
lumpkin4446 <liam.wener28@student.tamdistrict.org>
2026-08-31 04:42:31 +0000

    h b

--- topic-3-1/topic-3-4/sequence_practice.py
+++ topic-3-1/topic-3-4/sequence_practice.py
@@ -1,18 +1,32 @@
 scores = [72, 85, 91, 68, 88]
+
 title = "weekly score report"
 
 print(scores[0])
+
 print(scores[2])
+
 print(scores[-1])
 
 scores[1] = 86
+
 print(scores)
 
 scores.append(93)
+
 print(scores)
 
 first_word = title[0:6]
+
 last_word = title[13:18]
 
 print(first_word)
+
 print(last_word)
+
+label = last_word + ": " + str(len(scores))
+
+print(label)
+
+print("Scores:", scores)
+
